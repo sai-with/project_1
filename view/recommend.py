da31cc0b49fdd4e6390874d57ed28a83d8cef6b8
sai <kangjh9970114@gmail.com>
2023-07-28 11:27:30 +0900

    회의중 추천클래스  수정사항 반영

--- view/recommend.py
+++ view/recommend.py
@@ -202,6 +202,7 @@ class Recommend():
                 total_price2 = accom2[-1] + flight2[-1]
                 result['flight_fir'] = accom2[:-1] + flight2[:-1] + [total_price2]# 항공 우선 계획[(숙소), (가는편), (오는편)]
             
+            result['rent_fir'] = None
             # 연결 해제
             self.cursor.close()
             self.conn.close()
